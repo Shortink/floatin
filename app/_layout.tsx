acd0756126f6ff31309be6a75450c22d68b2c4d4
Shortink <21148306+Shortink@users.noreply.github.com>
2025-06-08 22:48:38 -0600

    add Linear gradient to root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,11 @@
-
 "use client";
 
 import { Slot, useRouter, usePathname } from "expo-router";
 import { useEffect, useState } from "react";
 import { ActivityIndicator, View } from "react-native";
 import { AuthProvider, useAuth } from "./../context/auth";
+import { LinearGradient } from "expo-linear-gradient";
+import Bubble from "../components/Bubble";
 
 function RootContent() {
   const router = useRouter();
@@ -12,36 +13,41 @@ function RootContent() {
   const { user } = useAuth();
   const [loading, setLoading] = useState(true);
 
-
   useEffect(() => {
     setLoading(false);
   }, []);
 
-  
-  const publicRoutes = ["/login", "/signup", "/welcome"];
+  const publicRoutes = ["/login", "/signup", "/welcome", "/reset"];
 
- 
   useEffect(() => {
     if (!loading) {
       if (!user && !publicRoutes.includes(pathname)) {
         router.replace("/welcome");
-      }
-     
-      else if (user && publicRoutes.includes(pathname)) {
-        router.replace("/(tabs)/home"); 
+      } else if (user && publicRoutes.includes(pathname)) {
+        const hasOnboarded = user.user_metadata?.has_completed_onboarding;
+
+        if (!hasOnboarded && pathname !== "/onboarding") {
+          router.replace("/onboarding");
+        } else if (hasOnboarded) {
+          router.replace("/(tabs)/home");
+        }
       }
     }
   }, [loading, user, pathname, router]);
 
   if (loading) {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <LinearGradient colors={["#f7f0ff", "#b9a5ec"]} style={{flex: 1}}>
         <ActivityIndicator size="large" />
-      </View>
+      </LinearGradient>
     );
   }
 
-  return <Slot />;
+  return (
+    <LinearGradient colors={["#f7f0ff", "#b9a5ec"]} style={{flex: 1}}>
+      <Slot />
+    </LinearGradient>
+  );
 }
 
 export default function RootLayout() {
