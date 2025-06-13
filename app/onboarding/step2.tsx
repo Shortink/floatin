cec0e411daaada96ab3f04f7347cb322bf5d55b7
Shortink <21148306+Shortink@users.noreply.github.com>
2025-06-12 21:13:34 -0600

    wip onboarding

--- app/onboarding/step2.tsx
+++ app/onboarding/step2.tsx
@@ -1,42 +1,121 @@
 import { useRouter } from "expo-router";
-import { View, StyleSheet } from "react-native";
-import Bubble from "../../components/Bubble";
+import { View, StyleSheet, Text } from "react-native";
 import Button from "../../components/Button";
 import { supabase } from "../../lib/supabase";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import PressableBubble from "../../components/ClickableBubble";
+import { useState } from "react";
 
 export default function SecondStepScreen() {
+  const [selected, setSelected] = useState<string[]>([]);
   const router = useRouter();
-
-  const finishOnboaring = async () => {
-    await supabase.auth.updateUser({
-      data: { has_completed_onboarding: true },
+  const options = ["Music", "Food", "Movies", "Sports", "Nature", "Gaming", "Arts"];
+  
+  const toggleOption = (option: string) => {
+    setSelected((prev) => {
+      // Check if the item is already in the array
+      const isSelected = prev.includes(option);
+      if (isSelected) {
+        // Remove it
+        return prev.filter((i) => i !== option);
+      } else {
+        // Add it
+        return [...prev, option];
+      }
     });
-    router.dismissTo("/(tabs)/home");
   };
 
-  const handleSignOut = async () => {
-    await supabase.auth.signOut();
-    router.replace("/welcome");
+  const fetchIntrest = async () => {
+    const { data, error } = await supabase
+      .from("interests")
+      .select("id, name, category");
+    if (error) console.error("Error fetching intrest list:", error);
+    console.log(data);
+
+    
   };
 
   return (
-    <View style={styles.container}>
-      <Button title="Finish" onPress={finishOnboaring} />
-      <Button title="Sign Out" onPress={handleSignOut} />
-    </View>
+    <KeyboardAwareScrollView
+      contentContainerStyle={{ flexGrow: 1, justifyContent: "center" }}
+      enableOnAndroid={true}
+      keyboardShouldPersistTaps="handled"
+    >
+      <View style={styles.container}>
+        <Text style={styles.title}>Help us match your energy</Text>
+        
+        <View style={styles.textContainer}>
+          <Text style={{textAlign: "center"}}>Pick everything that matches your vibe!</Text>
+        </View>
+        <View style={styles.grid}>
+          <Text style={{ width: "100%", textAlign: "center" }}>
+            What are you into?
+          </Text>
+          {options.map((option, index) => (
+            <PressableBubble
+              key={option}
+              title={option}
+              selected={selected.includes(option)}
+              onPress={() => toggleOption(option)}
+            />
+          ))}
+        </View>
+
+        <View style={styles.grid}>
+          <PressableBubble
+            selected={false}
+            title={"hello"}
+            onPress={() => fetchIntrest()}
+          />
+        </View>
+
+        <View style={styles.grid}>
+          {options.map((option) => (
+            <PressableBubble
+              key={option}
+              title={option}
+              selected={selected.includes(option)}
+              onPress={() => toggleOption(option)}
+            />
+          ))}
+        </View>
+      </View>
+    </KeyboardAwareScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
+    padding: 20,
   },
   title: {
-    fontSize: 32,
-    fontWeight: "bold",
+    fontSize: 36,
+    fontFamily: "Nunito",
+    fontWeight: "800",
     color: "#000",
     marginBottom: 40,
+    marginTop: 100,
+    textAlign: "center",
+  },
+  textContainer: {
+    backgroundColor: "#f7f0ff",
+    borderRadius: 10,
+    width: "100%",
+    marginBottom: 30,
+    paddingVertical: 10
+  },
+  grid: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    borderColor: "grey",
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+    // backgroundColor: "#f7f0ff",
+    marginBottom: 50,
+    width: "100%",
   },
 });
---
+++ app/onboarding/step3.tsx
@@ -0,0 +1,43 @@
+import { useRouter } from "expo-router";
+import { View, StyleSheet } from "react-native";
+import Bubble from "../../components/Bubble";
+import Button from "../../components/Button";
+import { supabase } from "../../lib/supabase";
+
+export default function ThirdStepScreen() {
+  const router = useRouter();
+
+  const finishOnboaring = async () => {
+    await supabase.auth.updateUser({
+      data: { has_completed_onboarding: true },
+    });
+    router.dismissTo("/(tabs)/home");
+  };
+
+  const handleSignOut = async () => {
+    await supabase.auth.signOut();
+    router.replace("/welcome");
+  };
+
+  return (
+    <View style={styles.container}>
+      <Button title="Finish" onPress={finishOnboaring} />
+      <Button title="Sign Out" onPress={handleSignOut} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 36,
+    fontFamily: "Nunito",
+    fontWeight: "800",
+    color: "#000",
+    marginBottom: 40,
+  },
+});
