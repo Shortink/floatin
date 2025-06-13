bbbdc7c98db92a1b6193f7d69f8fc5ae35291b95
Shortink <21148306+Shortink@users.noreply.github.com>
2025-06-13 01:45:31 -0600

    fix tab background

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,142 +7,142 @@ import MatchesIcon from "../../assets/icons/matches.svg";
 
 export default function AppTabsLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarActiveTintColor: "#66BFD2", // Dark color for active tab
-        tabBarInactiveTintColor: "#999", // Grey for inactive tab
-        tabBarStyle: {
-          borderTopLeftRadius: 20,
-          borderTopRightRadius: 20,
-          height: 100,
-          // overflow: "hidden",
-          // backgroundColor: "transparent",
-        },
-        tabBarLabelStyle: {
-          fontSize: 12,
-          fontWeight: "500",
-          marginBottom: 8,
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="home"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ focused }) => (
-            <View
-              style={{
-                alignItems: "center",
-                justifyContent: "flex-end",
-              }}
-            >
+    <View style={{ flex: 1, backgroundColor: "#D6BDFA" }}>
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+          tabBarActiveTintColor: "#66BFD2",
+          tabBarInactiveTintColor: "#999",
+          tabBarStyle: {
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+            height: 100,
+            overflow: "hidden",
+          },
+          tabBarLabelStyle: {
+            fontSize: 12,
+            fontWeight: "500",
+            marginBottom: 8,
+          },
+        }}
+      >
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "Home",
+            tabBarIcon: ({ focused }) => (
               <View
                 style={{
-                  height: 3,
-                  width: 35,
-                  backgroundColor: focused ? "#66BFD2" : "transparent",
-                  marginBottom: 4,
-                  borderRadius: 2,
+                  alignItems: "center",
+                  justifyContent: "flex-end",
                 }}
-              />
-              <HomeIcon
-                width={26}
-                height={26}
-                // fill={focused ? "#66BFD2" : "#999"}
-                fill={"#f00"}
-              />
-            </View>
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="events"
-        options={{
-          title: "Events",
-          tabBarIcon: ({ focused }) => (
-            <View
-              style={{
-                alignItems: "center",
-                justifyContent: "flex-end",
-              }}
-            >
+              >
+                <View
+                  style={{
+                    height: 3,
+                    width: 35,
+                    backgroundColor: focused ? "#66BFD2" : "transparent",
+                    marginBottom: 4,
+                    borderRadius: 2,
+                  }}
+                />
+                <HomeIcon
+                  width={26}
+                  height={26}
+                  fill={focused ? "#66BFD2" : "#999"}
+                />
+              </View>
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="events"
+          options={{
+            title: "Events",
+            tabBarIcon: ({ focused }) => (
               <View
                 style={{
-                  height: 3,
-                  width: 35,
-                  backgroundColor: focused ? "#66BFD2" : "transparent",
-                  marginBottom: 4,
-                  borderRadius: 2,
+                  alignItems: "center",
+                  justifyContent: "flex-end",
                 }}
-              />
-              <EventIcon
-                width={26}
-                height={26}
-                fill={focused ? "#66BFD2" : "#999"}
-              />
-            </View>
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="chat"
-        options={{
-          title: "Chat",
-          tabBarIcon: ({ focused }) => (
-            <View
-              style={{
-                alignItems: "center",
-                justifyContent: "flex-end",
-              }}
-            >
+              >
+                <View
+                  style={{
+                    height: 3,
+                    width: 35,
+                    backgroundColor: focused ? "#66BFD2" : "transparent",
+                    marginBottom: 4,
+                    borderRadius: 2,
+                  }}
+                />
+                <EventIcon
+                  width={26}
+                  height={26}
+                  fill={focused ? "#66BFD2" : "#999"}
+                />
+              </View>
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="chat"
+          options={{
+            title: "Chat",
+            tabBarIcon: ({ focused }) => (
               <View
                 style={{
-                  height: 3,
-                  width: 35,
-                  backgroundColor: focused ? "#66BFD2" : "transparent",
-                  marginBottom: 4,
-                  borderRadius: 2,
+                  alignItems: "center",
+                  justifyContent: "flex-end",
                 }}
-              />
-              <ChatIcon
-                width={26}
-                height={26}
-                fill={focused ? "#66BFD2" : "#999"}
-              />
-            </View>
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="likes"
-        options={{
-          title: "Matches",
-          tabBarIcon: ({ focused }) => (
-            <View
-              style={{
-                alignItems: "center",
-                justifyContent: "flex-end",
-              }}
-            >
+              >
+                <View
+                  style={{
+                    height: 3,
+                    width: 35,
+                    backgroundColor: focused ? "#66BFD2" : "transparent",
+                    marginBottom: 4,
+                    borderRadius: 2,
+                  }}
+                />
+                <ChatIcon
+                  width={26}
+                  height={26}
+                  fill={focused ? "#66BFD2" : "#999"}
+                />
+              </View>
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="likes"
+          options={{
+            title: "Matches",
+            tabBarIcon: ({ focused }) => (
               <View
                 style={{
-                  height: 3,
-                  width: 35,
-                  backgroundColor: focused ? "#66BFD2" : "transparent",
-                  marginBottom: 4,
-                  borderRadius: 2,
+                  alignItems: "center",
+                  justifyContent: "flex-end",
                 }}
-              />
-              <MatchesIcon
-                width={26}
-                height={26}
-                fill={focused ? "#66BFD2" : "#999"}
-              />
-            </View>
-          ),
-        }}
-      />
-    </Tabs>
+              >
+                <View
+                  style={{
+                    height: 3,
+                    width: 35,
+                    backgroundColor: focused ? "#66BFD2" : "transparent",
+                    marginBottom: 4,
+                    borderRadius: 2,
+                  }}
+                />
+                <MatchesIcon
+                  width={26}
+                  height={26}
+                  fill={focused ? "#66BFD2" : "#999"}
+                />
+              </View>
+            ),
+          }}
+        />
+      </Tabs>
+    </View>
   );
 }
