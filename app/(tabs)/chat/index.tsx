715fbe4c2eafd7b6a84b00cdb304b2e7963d354b
Shortink <21148306+Shortink@users.noreply.github.com>
2025-07-27 00:47:05 -0600

    chat setup

--- app/(tabs)/chat/index.tsx
+++ app/(tabs)/chat/index.tsx
@@ -9,10 +9,14 @@ import {
   FlatList,
   Image,
   Button,
+  Pressable,
+  TextInput,
 } from "react-native";
 import { router } from "expo-router";
 import { supabase } from "../../../lib/supabase";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Entypo from "@expo/vector-icons/Entypo";
+import { LinearGradient } from "expo-linear-gradient";
 
 type ChatPreview = {
   id: string;
@@ -26,6 +30,7 @@ type ChatPreview = {
 export default function ChatListScreen() {
   const [chats, setChats] = useState<ChatPreview[]>([]);
   const [loading, setLoading] = useState(true);
+  const [friend, setFriend] = useState("");
   useEffect(() => {
     fetchChats(true); // first load shows spinner
   }, []);
@@ -129,42 +134,89 @@ export default function ChatListScreen() {
   };
 
   const openChat = (chatId: string) => {
-    router.navigate(`/chat/${chatId}`);
+    // router.navigate(`/chat/${chatId}`);
+    router.navigate({
+      pathname: `/chat/${chatId}`,
+      params: {
+        displayName: chats.find((c) => c.id === chatId)?.name || "Chat",
+        avatarUrl: chats.find((c) => c.id === chatId)?.avatar || "",
+      },
+    });
   };
 
   if (loading) return <ActivityIndicator style={{ marginTop: 40 }} />;
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
-      <FlatList
-        data={chats}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <TouchableOpacity
-            onPress={() => openChat(item.id)}
-            style={styles.chatItem}
-          >
-            <Image source={{ uri: item.avatar }} style={styles.avatar} />
-            <View style={styles.chatText}>
-              <Text style={styles.title}>{item.name}</Text>
-              <Text style={styles.lastMessage}>
-                {item.last_message || "No messages yet"}
+    <View style={{ flex: 1, backgroundColor: "#FFF6F3" }}>
+      <LinearGradient colors={["#FFF6F3", "#D6BDFA"]} style={styles.topView}>
+        <View style={styles.headerRow}>
+          <Text style={{ fontSize: 24, fontWeight: "bold" }}>Chats</Text>
+        </View>
+      </LinearGradient>
+      <View style={{ padding: 10, paddingHorizontal: 20 }}>
+        <TextInput
+          style={styles.input}
+          placeholder="Search friends"
+          placeholderTextColor="#949494"
+          value={friend}
+          onChangeText={setFriend}
+        />
+      </View>
+      <View>
+        <FlatList
+          data={chats}
+          keyExtractor={(item) => item.id}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{
+            flexGrow: 1,
+            justifyContent: "center",
+            gap: 30,
+          }}
+          renderItem={({ item }) => (
+            <TouchableOpacity onPress={() => openChat(item.id)}>
+              <Image source={{ uri: item.avatar }} style={styles.pinAvatar} />
+              <Text
+                style={styles.pinName}
+                numberOfLines={2}
+                adjustsFontSizeToFit
+                minimumFontScale={0.5}
+              >
+                {item.name}{" "}
               </Text>
-            </View>
-          </TouchableOpacity>
-        )}
-      />
-
-      <Button
+            </TouchableOpacity>
+          )}
+        />
+      </View>
+        <FlatList
+          data={chats}
+          keyExtractor={(item) => item.id}
+          style={{ marginTop: 25 }}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              onPress={() => openChat(item.id)}
+              style={styles.chatItem}
+            >
+              <Image source={{ uri: item.avatar }} style={styles.avatar} />
+              <View style={styles.chatText}>
+                <Text style={styles.title}>{item.name}</Text>
+                <Text style={styles.lastMessage}>
+                  {item.last_message || "No messages yet"}
+                </Text>
+              </View>
+            </TouchableOpacity>
+          )}
+        />
+      {/* <Button
         onPress={makeGroupChats}
         title="Add all users to a group chat (for testing)"
         color="green"
       />
       <Button
         onPress={deleteGroupChat}
-        title="Delete all users to a group chat (for testing)"
+        title="Delete all users from group chat (for testing)"
         color="red"
-      />
-    </SafeAreaView>
+      /> */}
+    </View>
   );
 }
 
@@ -174,13 +226,30 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  headerRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+  },
+  topView: {
+    height: 100,
+    // borderBottomLeftRadius: 38,
+    borderRadius: 20,
+    padding: 10,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
   chatItem: {
     flexDirection: "row",
     paddingVertical: 14,
     paddingHorizontal: 16,
     borderBottomWidth: 1,
-    borderColor: "#eee",
-    backgroundColor: "white",
+    borderColor: "lavender",
+  },
+  pinName: {
+    textAlign: "center",
+    width: 70,
+    fontSize: 14,
   },
   button: {
     backgroundColor: "#007bff",
@@ -196,6 +265,12 @@ const styles = StyleSheet.create({
     marginRight: 12,
     backgroundColor: "#eee",
   },
+  pinAvatar: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    backgroundColor: "#eee",
+  },
   chatText: {
     flex: 1,
     justifyContent: "center",
@@ -207,7 +282,17 @@ const styles = StyleSheet.create({
   },
   lastMessage: {
     fontSize: 13,
-    color: "#666",
+    color: "grey",
     marginTop: 2,
+  },
+  input: {
+    width: "100%",
+    borderWidth: 1,
+    borderColor: "#D6BDFA",
+    height: 50,
+    borderRadius: 50,
+    marginBottom: 12,
+    fontSize: 15,
+    paddingHorizontal: 20,
   },
 });
